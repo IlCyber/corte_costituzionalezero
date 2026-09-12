e8137217a9eb1504b7a363dadb8ae1d500a409ea
BuildTools <unconfigured@null.spigotmc.org>
2026-09-12 23:38:46 +0200

    commit

---
+++ files/private/config_facsimile.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+/* Copiare questo file su Altervista e inserire i dati del database. */
+const DB_HOST = 'BOH';
+const DB_NAME = 'BOH';
+const DB_USER = 'BOH';
+const DB_PASSWORD = 'BOH';
+const SESSION_NAME = 'BOH';
+const MAX_STATE_BYTES = 'BOH';
+const GOOGLE_DRIVE_FOLDER_ID = 'BOH';
+const GOOGLE_CLIENT_ID = 'BOH';
+const GOOGLE_CLIENT_SECRET = 'BOH';
+const GOOGLE_REDIRECT_URI = 'BOH';
+const GOOGLE_WEBHOOK_URI = 'BOH';
+const GOOGLE_API_TIMEOUT = 'BOH';
