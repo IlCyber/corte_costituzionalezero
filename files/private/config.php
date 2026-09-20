a5227dac264a9951bf03a2b482e4742c6a0be7fb
BuildTools <unconfigured@null.spigotmc.org>
2026-09-20 19:03:17 +0200

    commit

--- files/private/config.php
+++ files/private/config.php
@@ -2,18 +2,18 @@
 declare(strict_types=1);
 
 /* Copiare questo file su Altervista e inserire i dati del database. */
-const DB_HOST = 'localhost';
-const DB_NAME = 'my_cortecostituzionalezero';
-const DB_USER = 'root';
-const DB_PASSWORD = '';
-const SESSION_NAME = 'cz_session';
-const MAX_STATE_BYTES = 15000000;
-const GOOGLE_DRIVE_FOLDER_ID = '165UMB_FmyzfQfsBypXJDj1lXUFuAEv0L';
-const GOOGLE_CLIENT_ID = '258222479900-aec10pkl76ijlq7qkevfp06suj56bkiu.apps.googleusercontent.com';
-const GOOGLE_CLIENT_SECRET = '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';
-const GOOGLE_REDIRECT_URI = 'https://cortecostituzionalezero.altervista.org/api.php?action=google_callback';
-const GOOGLE_WEBHOOK_URI = 'https://cortecostituzionalezero.altervista.org/api.php?action=google_webhook';
-const GOOGLE_API_TIMEOUT = 20;
+const DB_HOST = 'BOH';
+const DB_NAME = 'BOH';
+const DB_USER = 'BOH';
+const DB_PASSWORD = 'BOH';
+const SESSION_NAME = 'BOH';
+const MAX_STATE_BYTES = 'BOH';
+const GOOGLE_DRIVE_FOLDER_ID = 'BOH';
+const GOOGLE_CLIENT_ID = 'BOH';
+const GOOGLE_CLIENT_SECRET = 'BOH';
+const GOOGLE_REDIRECT_URI = 'BOH';
+const GOOGLE_WEBHOOK_URI = 'BOH';
+const GOOGLE_API_TIMEOUT = 'BOH';
 
 /*
  * Impostazioni facoltative: se mancano, api.php usa questi stessi valori
